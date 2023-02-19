47332bd25f5cabcf29b6d2d704b8a401321dd074
neagara <sbabayan133@gmail.com>
2023-02-19 21:27:38 +0400

    Jam

--- functions.py
+++ functions.py
@@ -1,3 +1,12 @@
-#write a function to calculate timezone difference
+#write a function to calculate timezone differenc
+from datetime import datetime
 def tiemezone_difference(country1, country2):
-    return 0
+     str_country1=country1
+     str_country2=country2
+     country1=datetime.strptime(str_country1, "%m/%d/%Y %H:%M:%S")
+     country2=datetime.strptime(str_country2, "%m/%d/%Y %H:%M:%S")
+     if country2>country1:
+          return country2-country1
+     else:     
+      return country1-country2
+     return f'Difference is {delta.days} days'
