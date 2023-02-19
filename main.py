ce50d57adff1a4cda19861c2c6577c4be14e5f92
vickerson <ruben.poghosyan@outlook.com>
2023-02-19 12:24:57 +0400

    refactor

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from utils import get_country_timezone, get_current_timezone
+from functions import tiemezone_difference
 
-print(get_current_timezone())
+#You can check an example of output 
+#print(get_country_timezone("Paris"))
+#country A
+A = input()
+#country B
+B = input()
+
+#get timezones
+A = get_current_timezone(A)
+B = get_country_timezone(B)
+difference = tiemezone_difference(A, B)
+print(difference)
